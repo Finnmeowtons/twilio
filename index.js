533058755374d8e4d3295ef4dd631f52f96acbe4
Finnmeowtons <soriano.christianjose.m@gmail.com>
2025-03-24 23:37:14 +0800

    phione number

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
-const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER;
+const twilioPhoneNumber = "+18312371528";
 const client = Twilio(accountSid, authToken);
 
 app.post('/send-otp', async (req, res) => {

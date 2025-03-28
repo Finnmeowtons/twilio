7414e183b81de9ec168ce224ea11718bcfbed9de
Vern_Pherus <145563323+VernPherus@users.noreply.github.com>
2025-03-28 21:53:24 +0800

    added SendGridAPI

--- index.js
+++ index.js
@@ -1,35 +1,78 @@
-require('dotenv').config();
-const express = require('express');
-const Twilio = require('twilio');
-const cors = require('cors');
-const req = require('express/lib/request');
+require("dotenv").config();
+const express = require("express");
+const Twilio = require("twilio");
+const cors = require("cors");
+const req = require("express/lib/request");
+
+//sendgrid
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const sendGridEmail = "nero.ferrer.up@phinmaed.com";
 
 const app = express();
 app.use(express.json());
 
-app.use(cors({
-  origin: '*', 
-  methods: ['GET', 'POST'], 
-}));
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST"],
+  })
+);
 
 const accountSid = "ACddd5d9ba010b2bcbc2c9c64c47c1e3c8";
 const authToken = "125d832ce40652700d1c0707ffef9c04";
 const twilioPhoneNumber = "+12185229850";
 const client = Twilio(accountSid, authToken);
 
-app.post('/send-otp', async (req, res) => {
+// sendgrid API
+app.post("/send-email-otp", async (req, res) => {
+  const { userEmail, otp } = req.body;
+
+  if (!userEmail || !otp) {
+    return res.status(400).json({error: "User Email and otp are required"})
+  }
+
+  const emailMsg = {
+    to: userEmail,
+    from: sendGridEmail,
+    subject: "Email Verification",
+    text: `OTP Number: ${otp}`,
+  };
+
+  try {
+    await sgMail.send(emailMsg);
+    console.log("Email sent");
+    res.status(200).json({ success: true });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error }); 
+  }
+
+  // sgMail
+  //   .send(emailMsg)
+  //   .then(() => {
+  //     console.log("Email sent");
+  //     res.status(200).json({ success: true });
+  //   })
+  //   .catch((error) => {
+  //     console.error(error);
+  //     res.status(400).json({ error: "Error with email" });
+  //   });
+});
+
+app.post("/send-otp", async (req, res) => {
   const { phone, otp } = req.body;
-console.log(phone + otp)
+  console.log(phone + otp);
   if (!phone || !otp) {
-    return res.status(400).json({ error: 'Phone number and OTP are required' });
+    return res.status(400).json({ error: "Phone number and OTP are required" });
   }
 
   try {
     client.messages.create({
-        body: otp,
-        from: twilioPhoneNumber,
-        to: phone
-    })
+      body: otp,
+      from: twilioPhoneNumber,
+      to: phone,
+    });
 
     res.status(200).json({ success: true });
   } catch (error) {
@@ -37,24 +80,20 @@ console.log(phone + otp)
   }
 });
 
-app.post("/call", async (req,res) => {
+app.post("/call", async (req, res) => {
   const { phone } = req.body;
   console.log(phone);
 
   if (!phone || !otp) {
-    return res.status(400).json({ error: 'Phone number is required' });
+    return res.status(400).json({ error: "Phone number is required" });
   }
 
-  
   const response = new VoiceResponse();
 
-  response.say('Tee tee');
+  response.say("Tee tee");
 
-  res.writeHead(200, { 'Content-Type': 'text/xml' });
+  res.writeHead(200, { "Content-Type": "text/xml" });
   res.end(response.toString());
+});
 
-
-
-})
-
-app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+app.listen(3000, () => console.log("Server running on http://localhost:3000"));
